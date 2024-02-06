3ebc5f1e5f76802410c9b9694eb36582f30dac74
Do Bruker <dorbruker1@gmail.com>
2024-02-06 20:45:59 +0200

    finished signin+login and UserContext

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,22 @@
 const express = require('express')
-const app = express();
 const userRoutes = require("./routes/userRoutes")
 const cvRoutes = require("./routes/cvRoutes")
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 
-app.use(cors());
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
-app.use(express.json())
+app.use(cors(corsOptions));
+
 
 app.use("/", userRoutes)
-app.use(":id/cv", cvRoutes)
+app.use("/:id/cv", cvRoutes)
 
 
 module.exports = app
--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,22 +1,97 @@
 const CV = require("../models/cvModel");
 const User = require("../models/userModel");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 exports.addUser = async (req, res) => {
   try {
-    const newUser = await User.create(req.body);
-    res.status(201).send(newUser);
-    console.log("User created:", newUser);
+    const userExists = await User.findOne({ username: req.body.username });
+    if (userExists) {
+      res.status(402).json("username already exists");
+    } else {
+      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+      const newUser = await User.create({
+        ...req.body,
+        password: hashedPassword,
+      });
+      res.status(201).json({
+        status: "success",
+        data: {
+          user: newUser,
+        },
+      });
+    }
   } catch (err) {
-    res.status(500).send(err);
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
   }
 };
 
-exports.getUser = async (req, res) => {
+const jwt = require("jsonwebtoken");
+const secret = "secretkey";
+exports.logInUser = async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.params.username });
-    res.send(user);
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({
+        status: "fail",
+        massage: "Inccorect password ",
+      });
+    } else {
+      const token = jwt.sign({ _id: user._id }, secret, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 60000,
+        sameSite: "strict",
+      });
+      res.status(200).send({
+        message: "Logged in successfully",
+        user,
+      });
+    }
   } catch (err) {
-    res.status(500).send(err);
+    console.error(err);
+    res.status(400).json({
+      status: "fail",
+      message: "An error occurred during login",
+    });
+  }
+};
+exports.authenticatedRoute = async (req, res) => {
+  try {
+    console.log(req.cookies.token);
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({ message: "No token provided." });
+    }
+
+    const decoded = jwt.verify(token, secret);
+    const userData = await User.findOne({ _id: decoded._id });
+
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ message: error.message || "An error occurred." });
+  }
+};
+
+exports.logoutUser = (req, res) => {
+  try {
+    res.cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: "strict",
+    });
+
+    res.status(200).send({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 };
 
@@ -44,24 +119,32 @@ exports.allcv = async (req, res) => {
   try {
     const userId = req.params.id;
     const user = await User.findById(userId).exec();
-    const cvIdsAsStrings = user.cv.map(cvId => cvId.toString());
-    const userCVs = []
+    const cvIdsAsStrings = user.cv.map((cvId) => cvId.toString());
+    const userCVs = [];
     for (let index = 0; index < cvIdsAsStrings?.length; index++) {
-         const cv = await CV.findById(cvIdsAsStrings[index]).exec();
-         console.log(cv);
-         userCVs.push(cv);
+      const cv = await CV.findById(cvIdsAsStrings[index]).exec();
+      console.log(cv);
+      userCVs.push(cv);
     }
-    res.send(userCVs)
+    res.send(userCVs);
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 exports.allUser = async (req, res) => {
-    try {
-      const users = await User.find({});
-      res.send(users);
-    } catch (err) {
-      res.status(500).send(err);
-    }
-  };
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+exports.findUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};

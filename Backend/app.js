6b59c53a53814f2ab572593e2d51f42b945750fe
Do Bruker <dorbruker1@gmail.com>
2024-02-06 08:44:22 +0200

    Context and login&sighin

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express();
 const userRoutes = require("./routes/userRoutes")
 const cvRoutes = require("./routes/cvRoutes")
+const cors = require('cors');
 
+app.use(cors());
 
 app.use(express.json())
 
 app.use("/", userRoutes)
-app.use("/:id/cv", cvRoutes)
-
+app.use(":id/cv", cvRoutes)
 
 
 module.exports = app
